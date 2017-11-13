32075df42d42d232e747cc933fd0ba23877f6f66
Yuiki <contact@yuiki.jp>
2017-11-13 17:01:08 +0900

    :sparkle: introduce gradle

---
+++ src/main/kotlin/Course.kt
@@ -0,0 +1,13 @@
+data class Course @JvmOverloads constructor(
+        var number: String? = null,
+        var name: String? = null,
+        var classMethod: String? = null,
+        var credits: String? = null,
+        var year: String? = null,
+        var term: String? = null,
+        var weekdayAndPeriod: String? = null,
+        var classRoom: String? = null,
+        var instructor: String? = null,
+        var overview: String? = null,
+        var remarks: String? = null
+)
