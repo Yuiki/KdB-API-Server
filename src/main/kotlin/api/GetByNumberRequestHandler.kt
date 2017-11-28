77467d35edca529b3e1594b3e54493871b1591f8
Yuiki <contact@yuiki.jp>
2017-11-28 14:22:37 +0900

    :sparkle: make CourseRepository

--- src/main/kotlin/api/GetByNumberRequestHandler.kt
+++ src/main/kotlin/api/GetByNumberRequestHandler.kt
@@ -3,11 +3,11 @@ package api
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import db.Course
+import db.CourseRepository
 
 class GetByNumberRequestHandler : RequestHandler<String, Course> {
     override fun handleRequest(number: String, context: Context): Course {
-        Course.getConnection().use {
-            return Course.getByNumber(number, it)
-        }
+        val repo = CourseRepository()
+        return repo.getByNumber(number)
     }
 }
